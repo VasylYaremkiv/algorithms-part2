431130e2bf00a264dc8ad278d6d08bd166f9e6f6
Vasyl Yaremkiv <vasyl.yaremkiv@ge.com>
2019-12-26 15:57:12 +0200

    implement calculation energies

--- week2/SeamCarver.java
+++ week2/SeamCarver.java
@@ -1,9 +1,12 @@
 import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.StdOut;
+
 import java.awt.Color;
 
 
 public class SeamCarver {
     private Picture picture;
+    private double[][] energies;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
@@ -11,8 +14,8 @@ public class SeamCarver {
             throw new IllegalArgumentException();
         }
 
-
         this.picture = picture;
+        this.energies = calculateEnergies();
     }
  
     // current picture
@@ -39,9 +42,7 @@ public class SeamCarver {
             throw new IllegalArgumentException();
         }
 
-
-
-        return 0.0;
+        return energies[x][y];
     }
  
     // sequence of indices for horizontal seam
@@ -97,6 +98,7 @@ public class SeamCarver {
         }
 
         this.picture = newPicture;
+        this.energies = calculateEnergies();
     }
  
     // remove vertical seam from current picture
@@ -139,10 +141,50 @@ public class SeamCarver {
         }
 
         this.picture = newPicture;
+        this.energies = calculateEnergies();
+    }
+
+    private double[][] calculateEnergies() {
+        double[][] result = new double[this.width()][this.height()];
+        Color nextI, prevI, nextJ, prevJ;
+
+        for (int i = 0; i < this.width(); i++) {
+            for (int j = 0; j < this.height(); j++) {
+                if (i == 0 || i == this.width() - 1 || j == 0 || j == this.height() - 1) {
+                    result[i][j] = 1000.0;
+                    continue;
+                }
+
+                prevI = picture.get(i - 1, j);
+                nextI = picture.get(i + 1, j);
+                prevJ = picture.get(i, j - 1);
+                nextJ = picture.get(i, j + 1);
+                
+                result[i][j] = Math.sqrt(
+                    Math.pow(prevI.getGreen() - nextI.getGreen(), 2) +
+                    Math.pow(prevI.getRed() - nextI.getRed(), 2) +
+                    Math.pow(prevI.getBlue() - nextI.getBlue(), 2) +
+
+                    Math.pow(prevJ.getGreen() - nextJ.getGreen(), 2) +
+                    Math.pow(prevJ.getRed() - nextJ.getRed(), 2) +
+                    Math.pow(prevJ.getBlue() - nextJ.getBlue(), 2)
+                );
+            }
+        }
+
+        return result;
     }
  
     //  unit testing (optional)
     public static void main(String[] args) {
+        Picture p = new Picture("6x5.png");
+        SeamCarver s = new SeamCarver(p);
 
+        for (int i = 0; i < s.width(); i++) {
+            for (int j = 0; j < s.height(); j++) {
+                StdOut.print(String.format("%8.2f  ", s.energy(i, j)));                
+            }
+            StdOut.println();
+        }
     }
  }
