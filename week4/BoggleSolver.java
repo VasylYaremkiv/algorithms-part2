73f1aa9524fed35e318c4647286be885f4685933
Vasyl Yaremkiv <vasyl.yaremkiv@ge.com>
2020-01-02 15:56:00 +0200

    add skeleton for boggle solver

---
+++ week4/BoggleSolver.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class BoggleSolver {
+    private final List<String> dictionary;
+    // Initializes the data structure using the given array of strings as the dictionary.
+    // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+    public BoggleSolver(String[] dictionary) {
+        this.dictionary = Arrays.asList(dictionary);
+    }
+
+    // Returns the set of all valid words in the given Boggle board, as an Iterable.
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+
+
+    }
+
+    // Returns the score of the given word if it is in the dictionary, zero otherwise.
+    // (You can assume the word contains only the uppercase letters A through Z.)
+    public int scoreOf(String word) {
+        if (!dictionary.contains(word)) {
+            return 0;
+        }
+
+        
+
+        return 0;
+    }
+
+
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        BoggleBoard board = new BoggleBoard(args[1]);
+        int score = 0;
+        for (String word : solver.getAllValidWords(board)) {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);
+    }
+
+}
