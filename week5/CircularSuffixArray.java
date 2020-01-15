bc22cdf7745534b82c85fdb4b7976e718635b5e8
Vasyl Yaremkiv <vyaremkiv@solanolabs.com>
2020-01-15 23:12:42 +0200

    implement CircularSuffixArray

--- week5/CircularSuffixArray.java
+++ week5/CircularSuffixArray.java
@@ -1,10 +1,11 @@
 import edu.princeton.cs.algs4.StdOut;
-
+import java.util.Arrays;
 
 
 public class CircularSuffixArray {
     private final String s;
     private final int length;
+    private int[] indexes;
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
@@ -14,6 +15,16 @@ public class CircularSuffixArray {
 
         this.s = s;
         this.length = s.length();
+
+        CircularSuffix[] array = new CircularSuffix[this.length ];
+        for (int i = 0; i < this.length; i++) {
+            array[i] = new CircularSuffix(s, i);
+        }
+        Arrays.sort(array);
+        this.indexes = new int[this.length];
+        for (int i = 0; i < this.length; i++) {
+            this.indexes[i] = array[i].offset;
+        }
     }
 
     // length of s
@@ -27,7 +38,41 @@ public class CircularSuffixArray {
             throw new IllegalArgumentException();
         }
 
-        return 0;
+        return this.indexes[i];
+    }
+
+    private class CircularSuffix implements Comparable<CircularSuffix> {
+        private String s;
+        private int offset;
+
+        public CircularSuffix(String s, int offset) {
+            this.s = s;
+            this.offset = offset;
+        }
+
+        private char charAt(int pos) {
+            return s.charAt((offset + pos) % s.length());
+        }
+
+        public int compareTo(CircularSuffix other) {
+            if (this == other) {
+                return 0;
+            }
+
+            for (int i = 0; i < length; i++) {
+                if (this.charAt(i) > other.charAt(i)) {
+                    return 1;
+                }
+                if (this.charAt(i) < other.charAt(i)) {
+                    return -1;
+                }
+            }
+
+            return 0;
+        }
+        public String toString() {
+            return s.substring(offset, s.length()) + s.substring(0, offset);
+        }
     }
 
     // unit testing (required)
